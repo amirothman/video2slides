e81a30ffacda8676e26fc049e096e3ec9d1ec7ac
Amir Othman <qowi3456@uni-weimar.de>
2015-07-11 08:41:33 +0200

    update

--- split_analysis.py
+++ split_analysis.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 folder_name = sys.argv[1]
 output_folder = sys.argv[2]
+#folder_name = 'tmp-ffmpeg-folder'
+#output_folder = 'tmp-output-folder'
 images = [ folder_name+'/'+file_name for file_name in os.listdir(folder_name)]
 
 arr = []
@@ -17,15 +19,21 @@ for i in range(len(images)-1):
   a = cv2.imread(pair[0])
   b = cv2.imread(pair[1])
   n = (np.linalg.norm(b-a))*10000/(a.shape[0]*a.shape[1])
+#  sys.stdout.write('.')
   element = {'norm':n,'file_1':pair[0],'file_2':pair[1]}
   arr.append(element)
 
 df = pd.DataFrame(arr)
-seventy_five = df['norm'].describe()[6]
 
-chosen = df[df['norm'] > df.norm.quantile(0.75)]
-
-#http://stackoverflow.com/questions/11854847/display-an-image-from-a-file-in-an-ipython-notebook
+chosen = df[df['norm'] > df.norm.quantile(0.95)]
+# 
+# quantile_var = 0.9
+# 
+# 
+# while chosen.size > 200:
+#   quantile_var = quantile_var + 0.05
+#   chosen = df[df['norm'] > df.norm.quantile(quantile_var)]
+# 
 print '#!/bin/bash'
 for item in chosen['file_2'].iteritems():
   print('cp %(filename)s %(output_folder)s'%{'filename':item[1],'output_folder':output_folder})
--- video2slides.sh
+++ video2slides.sh
@@ -5,7 +5,7 @@ youtube-dl $1 -f best
 mkdir tmp-ffmpeg-folder
 mkdir tmp-output-folder
 
-ffmpeg -i *.mp4 -vf fps=1 tmp-ffmpeg-folder/out%05d.png
+ffmpeg -threads 4 -i *.mp4 -vf fps=1 tmp-ffmpeg-folder/out%05d.png
 
 python split_analysis.py tmp-ffmpeg-folder tmp-output-folder > script_for_copying.sh
 
@@ -13,7 +13,7 @@ sh script_for_copying.sh
 
 convert tmp-output-folder/*.png -trim slide.pdf
 
-rm -R tmp-ffmpeg-folder
-rm -R tmp-output-folder
-rm script_for_copying.sh
-rm *.mp4
+#rm -R tmp-ffmpeg-folder
+#rm -R tmp-output-folder
+#rm script_for_copying.sh
+#rm *.mp4
